66f67e45da3810ba858cb885f1773d9393a4fac0
ovechkin-dm <ovechkin.dm@gmail.com>
2025-04-16 14:40:29 +0200

    lint

--- pkg/dyno/api.go
+++ pkg/dyno/api.go
@@ -13,28 +13,27 @@ import (
 func Dynamic[T any](handler func(m reflect.Method, values []reflect.Value) []reflect.Value, opts ...dynoconfig.Option) (T, error) {
 	ifaceInstance := new(T)
 	t := reflect.ValueOf(ifaceInstance).Elem().Type()
-	result, err := DynamicByType( handler, t, opts...)
-
+	result, err := DynamicByType(handler, t, opts...)
 	if err != nil {
 		return *ifaceInstance, err
 	}
 	iface, ok := result.(T)
-	if !ok {	
+	if !ok {
 		return *ifaceInstance, errors.New("result is not of type T")
 	}
 	return iface, nil
 }
 
 func DynamicByType(
-	handler func(m reflect.Method, values []reflect.Value) []reflect.Value, 
-	t reflect.Type, 
+	handler func(m reflect.Method, values []reflect.Value) []reflect.Value,
+	t reflect.Type,
 	opts ...dynoconfig.Option,
 ) (any, error) {
 	cfg := dynoconfig.Config{}
 	for _, opt := range opts {
 		opt(&cfg)
-	}	
-	result, err :=  proxy.Create(func(m reflect.Method, values []reflect.Value) []reflect.Value {
+	}
+	result, err := proxy.Create(func(m reflect.Method, values []reflect.Value) []reflect.Value {
 		return handler(m, values)
 	}, &cfg, t)
 	if err != nil {
